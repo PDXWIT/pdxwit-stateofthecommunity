4d5789ee8daf90284bd293d58a649d2cac01a99e
Ilana Davis <ilanadavis@Ilanas-MacBook-Pro.local>
2018-06-27 16:27:11 -0700

    add data for salary chart

--- js/chartjs_salary_data.js
+++ js/chartjs_salary_data.js
@@ -2,4 +2,31 @@
 // =====================
 // Salary Chart
 // =====================
-var ctx = document.getElementById("salary").getContext("2d");
+var ctxSalary = document.getElementById("salary").getContext("2d");
+
+var salaryData = {
+  labels: [
+    "Yes",
+    "No"
+  ],
+  datasets: [
+    {
+      data: [13, 87],
+      backgroundColor: [
+        '#FFFF8B',
+        '#FF4C4C',
+      ]
+    }]
+};
+
+var salaryDoughnut = new Chart(ctxSalary, {
+  type: 'doughnut',
+  data: salaryData,
+  options: {
+    percentageInnerCutout : 50,
+    animationEasing : "easeOutBounce",
+    animateRotate : true,
+    responsive: true,
+    maintainAspectRatio: true
+  }
+});
