0b8b8e657984bd627a36317f8fb0be85c6960c75
Ilana Davis <ilanadavis@Ilanas-MacBook-Pro.local>
2018-06-27 09:11:06 -0700

    add missing labels for benefits chart. comment out unused label/data points

--- js/chartjs_data.js
+++ js/chartjs_data.js
@@ -2,10 +2,10 @@ var ctx = document.getElementById("benefits").getContext("2d");
 var myChart = new Chart(ctx, {
     type: 'horizontalBar',
     data: {
-        labels: ["Paid maternity leave", "Paid paternity leave", "Nursing room", "Flexible hours", "Remote working", "Onsite childcare", "Trans healthcare", "None of these"],
+        labels: ["Paid maternity leave", "Paid paternity leave", "Nursing room", "Flexible hours", "Remote working", "Onsite childcare", /*"Paid sick leave", "Paid vacation", */ "Trans healthcare", "None of these"],
         datasets: [{
             label: 'Percent',
-            data: [67, 48, 55, 72, 70, 6, 76, 89, 22, 4],
+            data: [67, 48, 55, 72, 70, 6, /*76, 89,*/ 22, 4],
             backgroundColor: [
                 '#4583A2',
                 '#4583A2',
