2261d4ce852548ea1ba2ca1c219d86f93dd9b152
Ilana Davis <ilanadavis@Ilanas-MacBook-Pro.local>
2018-06-27 14:59:25 -0700

    align bubble chart. Add text to bubbles.

--- js/chartjs_culture_data.js
+++ js/chartjs_culture_data.js
@@ -3,14 +3,11 @@
 // Culture Chart
 // =====================
 var ctxCulture = document.getElementById("culture");
-
-
-
 var cultureData = {
   labels: 'Culture',
   datasets: [
     {
-      label: ['Diverse'],
+      label: ['Diverse 34%'], //34
       data: [{
         x: 85,
         y: 4,
@@ -18,39 +15,39 @@ var cultureData = {
     }],
     backgroundColor: '#536028',
   }, {
-    label: ['Inclusive'], //54
+    label: ['Inclusive 54%'], //54
     data: [{
-      x: 40,
+      x: 45,
       y: 45,
       r: 115
     }],
     backgroundColor: '#536028',
   }, {
-    label: ['Toxic'], //9
+    label: ['Toxic 9%'], //9
     data: [{
       x: 50,
-      y: 15,
+      y: 6,
       r: 72
     }],
     backgroundColor: '#536028',
   }, {
-    label: ['Bro-Culture'], //18
+    label: ['Bro-Culture 18%'], //18
     data: [{
-      x: 22,
-      y: 52,
+      x: 25,
+      y: 55,
       r: 79
     }],
     backgroundColor: '#536028',
   }, {
-    label: ['Results-Oriented'], //63
+    label: ['Results-Oriented 63%'], //63
     data: [{
       x: 78,
-      y: 40,
+      y: 47,
       r: 125
     }],
     backgroundColor: '#536028',
   }, {
-    label: ['Caring'], //51
+    label: ['Caring 51%'], //51
     data: [{
       x: 30,
       y: 10,
@@ -58,10 +55,10 @@ var cultureData = {
     }],
     backgroundColor: '#536028',
   }, {
-    label: ['Other'], //15
+    label: ['Other 15%'], //15
     data: [{
-      x: 55,
-      y: 44,
+      x: 63,
+      y: 24,
       r: 76
     }],
     backgroundColor: '#536028',
@@ -73,6 +70,7 @@ var bubbleChart = new Chart(ctxCulture, {
   type: 'bubble',
   data: cultureData,
   options: {
+    responsive: true,
     layout: {
       padding: 70,
     },
@@ -100,4 +98,33 @@ var bubbleChart = new Chart(ctxCulture, {
       }]
     },
   },
+});
+
+// Define a plugin to provide data labels
+Chart.plugins.register({
+  afterDatasetsDraw: function(ctxCulture, easing) {
+    // To only draw at the end of animation, check for easing === 1
+    var ctx = bubbleChart.chart.ctx;
+    bubbleChart.data.datasets.forEach(function (dataset, i) {
+      var meta = bubbleChart.getDatasetMeta(i);
+      if (!meta.hidden) {
+        meta.data.forEach(function(element, index) {
+          // Draw the text in black, with the specified font
+          ctx.fillStyle = '#FFF';
+          var fontSize = 18;
+          var fontStyle = 'normal';
+          var fontFamily = 'Helvetica Neue';
+          ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
+          // Convert to string
+
+          var dataString = dataset.label[index].toString();
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          var padding = 5;
+          var position = element.tooltipPosition();
+          ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
+        });
+      }
+    });
+  }
 });
