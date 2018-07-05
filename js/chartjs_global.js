f7136dd48f9bb47433259c19c06cb981e335dcc1
Ilana Davis <ilanadavis@Ilanas-MacBook-Pro.local>
2018-07-05 15:15:20 -0700

    add default font size and font family to charts

--- js/chartjs_global.js
+++ js/chartjs_global.js
@@ -1,2 +1,4 @@
 Chart.defaults.global.defaultFontColor = 'white';
 Chart.defaults.global.legend.display = false;
+Chart.defaults.global.defaultFontFamily = "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
+Chart.defaults.global.defaultFontSize = 12;
