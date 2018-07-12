418c4d264ad312a01de967bb3e833405a0d4f5aa
Ilana Davis <ilanadavis@Ilanas-MacBook-Pro.local>
2018-07-12 13:05:43 -0700

    remove font size from charts and allow default. Add padding around charts

--- js/chartjs_global.js
+++ js/chartjs_global.js
@@ -1,4 +1,14 @@
 Chart.defaults.global.defaultFontColor = 'white';
 Chart.defaults.global.legend.display = false;
 Chart.defaults.global.defaultFontFamily = "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
-Chart.defaults.global.defaultFontSize = 16;
+// Chart.defaults.global.defaultFontSize = 16;
+
+//
+// window.onload = function() {
+//   if (window.matchMedia("(max-width: 576)").matches) {
+//     Chart.defaults.global.defaultFontSize = 9;
+//   }
+//   else {
+//     Chart.defaults.global.defaultFontSize = 16;
+//   }
+// }
