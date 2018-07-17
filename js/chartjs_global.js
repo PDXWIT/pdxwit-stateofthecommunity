0cba925a08ab12889ba6f14d28566c0f00f8d2ac
Ilana Davis <ilanadavis@Ilanas-MacBook-Pro.local>
2018-07-17 14:00:31 -0700

    add transition to carousel

--- js/chartjs_global.js
+++ js/chartjs_global.js
@@ -3,7 +3,7 @@ Chart.defaults.global.legend.display = false;
 Chart.defaults.global.defaultFontFamily = "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
 // Chart.defaults.global.defaultFontSize = 9;
 
-window.onload = function() {
+function matchMedia() {
   if (window.matchMedia("(min-width: 768)").matches) {
     Chart.defaults.global.defaultFontSize = 30;
   }
