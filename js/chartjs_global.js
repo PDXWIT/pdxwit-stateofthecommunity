13efab98d699916ade6e7819515e3d5c736fb06d
Ilana Davis <ilanadavis@Ilanas-MacBook-Pro.local>
2018-07-24 10:49:02 -0700

    change default chart text size

--- js/chartjs_global.js
+++ js/chartjs_global.js
@@ -1,13 +1,13 @@
 Chart.defaults.global.defaultFontColor = 'white';
 Chart.defaults.global.legend.display = false;
 Chart.defaults.global.defaultFontFamily = "'Times New Roman', 'Times', 'Arial', serif";
-// Chart.defaults.global.defaultFontSize = 9;
+Chart.defaults.global.defaultFontSize = 14;
 
-function matchMedia() {
-  if (window.matchMedia("(min-width: 768)").matches) {
-    Chart.defaults.global.defaultFontSize = 30;
-  }
-  else {
-    Chart.defaults.global.defaultFontSize = 6;
-  }
-}
+// function matchMedia() {
+//   if (window.matchMedia("(min-width: 768)").matches) {
+//     Chart.defaults.global.defaultFontSize = 30;
+//   }
+//   else {
+//     Chart.defaults.global.defaultFontSize = 6;
+//   }
+// }
