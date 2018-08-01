5398b1c3bcc0f8d5f69bd27d91908dc1710876fb
Ilana Davis <ilanadavis@Ilanas-MacBook-Pro.local>
2018-08-01 11:57:16 -0700

    correct path for proxima nova font. Add files for league gothic font. Update charts font family.

--- js/chartjs_global.js
+++ js/chartjs_global.js
@@ -1,3 +1,3 @@
 Chart.defaults.global.defaultFontColor = 'white';
 Chart.defaults.global.legend.display = false;
-Chart.defaults.global.defaultFontFamily = "'ProximaNova-Regular'";
+Chart.defaults.global.defaultFontFamily = "'proximanova-regular'";
