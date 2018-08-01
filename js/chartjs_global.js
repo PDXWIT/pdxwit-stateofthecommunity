c4efcd762e000a726ba54e8afbc37afc9ec55cac
Ilana Davis <ilanadavis@Ilanas-MacBook-Pro.local>
2018-08-01 10:16:06 -0700

    remove commented out code. Add fonts for proxima nova to work on various devices

--- js/chartjs_global.js
+++ js/chartjs_global.js
@@ -1,12 +1,3 @@
 Chart.defaults.global.defaultFontColor = 'white';
 Chart.defaults.global.legend.display = false;
 Chart.defaults.global.defaultFontFamily = "'ProximaNova-Regular'";
-
-// function matchMedia() {
-//   if (window.matchMedia("(min-width: 768)").matches) {
-//     Chart.defaults.global.defaultFontSize = 30;
-//   }
-//   else {
-//     Chart.defaults.global.defaultFontSize = 6;
-//   }
-// }
